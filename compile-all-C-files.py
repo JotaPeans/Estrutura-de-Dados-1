a2fcecbcbb07cdc11727ee93b292100340edbd88
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2023-03-30 19:46:53 -0300

    add compile one parsing argv argument

--- compile-all-C-files.py
+++ compile-all-C-files.py
@@ -1,5 +1,7 @@
 import os
+from sys import argv
 
+os.system("rmdir /s dist")
 os.system("mkdir dist")
 
 def getPathCFile(n):
@@ -14,20 +16,29 @@ def getPathCFile(n):
 def getPathDistFile():
     return os.path.abspath("./dist")
 
+if(not argv[1]):
+    progress = 0
+    for x in range(1, 21+1):
+        progress += 100/21
 
-progress = 0
-for x in range(1, 21+1):
-    progress += 100/21
+        if(x < 10):
+            os.system(f"gcc \"{getPathCFile(x)}\" -o \"{getPathDistFile()}\"/Questao0{x}.exe")
+        else:
+            os.system(f"gcc \"{getPathCFile(x)}\" -o \"{getPathDistFile()}\"/Questao{x}.exe")
 
-    if(x < 10):
-        os.system(f"gcc \"{getPathCFile(x)}\" -o \"{getPathDistFile()}\"/Questao0{x}.exe")
-    else:
-        os.system(f"gcc \"{getPathCFile(x)}\" -o \"{getPathDistFile()}\"/Questao{x}.exe")
+        os.system("cls")
+        print(f"progresso: {int(progress)}%")
 
     os.system("cls")
-    print(f"progresso: {int(progress)}%")
+    print(f"progresso: 100%\n")
+    print("Todos os arquivos foram compilados com sucesso!")
+    print("Disponível em ./dist")
 
-os.system("cls")
-print(f"progresso: 100%\n")
-print("Todos os arquivos foram compilados com sucesso!")
-print("Disponível em ./dist")
+else:
+    try:
+        if(int(argv[1]) < 10):
+            os.system(f"gcc \"{getPathCFile(int(argv[1]))}\" -o \"{getPathDistFile()}\"/Questao0{argv[1]}.exe")
+        else:
+            os.system(f"gcc \"{getPathCFile(int(argv[1]))}\" -o \"{getPathDistFile()}\"/Questao{argv[1]}.exe")
+    except:
+        print("Insira apenas numeros como argumento")
